a6e274f66cc712de548225593c6c2b11de9abcb3
James Lott <james@lottspot.com>
2023-10-14 20:25:09 -0400

    git-configpack/tests/packlib-path.mk: add test suite "packlib-path"

---
+++ tests/packlib-path.mk
@@ -0,0 +1,12 @@
+SUITES        += packlib-path
+LIBPATH_TESTS += packlib-path-realpath
+TESTS         += $(LIBPATH_TESTS)
+
+packlib-path: $(LIBPATH_TESTS)
+
+packlib-path-realpath:
+	source ../share/install.sh && if path_realpath ''; then false; fi # an empty patharg should return nonzero
+	source ../share/install.sh && if path_realpath /$(shell dd if=/dev/urandom bs=1 count=15 2>/dev/null | base64 -w0); then false; fi # non-existent paths should return nonzero
+	source ../share/install.sh && test "`path_realpath .`" = '$(shell realpath `pwd` 2>/dev/null || readlink `pwd` 2>/dev/null || pwd)'
+	source ../share/install.sh && test "`path_realpath /`" = '/'
+	source ../share/install.sh && path_realpath /bin
