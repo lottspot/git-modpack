9f2bc3a29f29f4e2b8ab545f3005d3865bad359d
James Lott <james@lottspot.com>
2023-10-16 17:58:24 -0400

    git-configpack: add alias "configpack-newalias"

--- install.sh
+++ install.sh
@@ -1,9 +1,23 @@
 #!/usr/bin/env bash
 
 prog_dir=$(dirname "$0")
+
+if [[ $BASH_SOURCE ]] && [[ $0 != $BASH_SOURCE ]]; then
+  prog_dir=$(dirname "$BASH_SOURCE")
+  prog_return=1
+fi
+
 prog_template=$prog_dir/share/install.sh
 prog_properties=$prog_dir/install.properties
+
 source "$prog_template"
+
+if [[ $prog_return ]]; then
+  PACKDIR=$prog_dir
+  ABSPACKDIR=$(path_realpath "$PACKDIR")
+  return 0
+fi
+
 properties_load "$prog_properties"
 exec bash -c "$(sed                                  \
   -e "s,%pack_name%,${PROPERTIES['package.name']},g" \
