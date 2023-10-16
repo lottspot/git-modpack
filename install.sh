41ec4a5a6b2ea3a9eaf804f3db87b88a713b8e75
James Lott <james@lottspot.com>
2023-10-15 22:42:57 -0400

    git-configpack/install.sh: use path_realpath for portability

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ prog_template=$prog_dir/share/install.sh
 prog_properties=$prog_dir/install.properties
 source "$prog_template"
 properties_load "$prog_properties"
-exec bash -c "$(sed                     \
+exec bash -c "$(sed                                  \
   -e "s/%pack_name%/${PROPERTIES['package.name']}/g" \
   -e "s/%pack_version%/$(cat "$prog_dir/VERSION")/g"  < "$prog_template"
-  )" "$(realpath "$0")" "$@"
+  )" "$(path_realpath "$0")" "$@"
