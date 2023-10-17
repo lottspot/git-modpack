9863e564dfebc77c0d58b6bda255f8d19e4f820d
James Lott <james@lottspot.com>
2023-10-17 14:33:04 -0400

    git-configpack/install.sh: fix sed delimiter to use ","
    Using "/" as the sed delimiter causes problems working with version
    strings which contain that character (as conifgpack version strings do).

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ prog_properties=$prog_dir/install.properties
 source "$prog_template"
 properties_load "$prog_properties"
 exec bash -c "$(sed                                  \
-  -e "s/%pack_name%/${PROPERTIES['package.name']}/g" \
-  -e "s/%pack_version%/$(cat "$prog_dir/VERSION")/g"  < "$prog_template"
+  -e "s,%pack_name%,${PROPERTIES['package.name']},g" \
+  -e "s,%pack_version%,$(cat "$prog_dir/VERSION"),g"  < "$prog_template"
   )" "$(path_realpath "$0")" "$@"
