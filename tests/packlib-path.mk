6e29aa1604a5d6e2964534152c9289fad6e1af75
James Lott <james@lottspot.com>
2024-01-06 18:30:25 -0500

    tests/packlib-path.mk: refactor for lowercase locals convention

--- tests/packlib-path.mk
+++ tests/packlib-path.mk
@@ -1,24 +1,26 @@
+libpath_tests += packlib-path-realpath
+libpath_tests += packlib-path-relto
 SUITES        += packlib-path
-LIBPATH_TESTS += packlib-path-realpath
-LIBPATH_TESTS += packlib-path-relto
-TESTS         += $(LIBPATH_TESTS)
+TESTS         += $(libpath_tests)
 
-packlib-path: $(LIBPATH_TESTS)
+libpath_run := source ../install.sh &&
+
+packlib-path: $(libpath_tests)
 
 packlib-path-realpath:
-	source ../install.sh && if path_realpath ''; then false; fi # an empty patharg should return nonzero
-	source ../install.sh && if path_realpath /$(shell dd if=/dev/urandom bs=1 count=15 2>/dev/null | base64 -w0); then false; fi # non-existent paths should return nonzero
-	source ../install.sh && test "`path_realpath .`" = '$(shell realpath `pwd` 2>/dev/null || readlink `pwd` 2>/dev/null || pwd)'
-	source ../install.sh && test "`path_realpath /`" = '/'
-	source ../install.sh && path_realpath /bin
+	$(libpath_run) if path_realpath ''; then false; fi # an empty patharg should return nonzero
+	$(libpath_run) if path_realpath /$(shell dd if=/dev/urandom bs=1 count=15 2>/dev/null | base64 -w0); then false; fi # non-existent paths should return nonzero
+	$(libpath_run) test "`path_realpath .`" = '$(shell realpath `pwd` 2>/dev/null || readlink `pwd` 2>/dev/null || pwd)'
+	$(libpath_run) test "`path_realpath /`" = '/'
+	$(libpath_run) path_realpath /bin
 
 packlib-path-relto:
-	source ../install.sh && test "`path_relto .. .`" = ..
-	source ../install.sh && test "`path_relto . ..`" = ./tests
-	source ../install.sh && test "`path_relto . .`"  = .
-	source ../install.sh && test "`path_relto / /`"  = .
-	source ../install.sh && test "`path_relto sub2/filename sub1`" = ../sub2/filename
-	source ../install.sh && echo "`path_relto / .`"
-	source ../install.sh && echo "`path_relto . /`"
-	source ../install.sh && echo "`path_relto / ..`"
-	source ../install.sh && echo "`path_relto .. /`"
+	$(libpath_run) test "`path_relto .. .`" = ..
+	$(libpath_run) test "`path_relto . ..`" = ./tests
+	$(libpath_run) test "`path_relto . .`"  = .
+	$(libpath_run) test "`path_relto / /`"  = .
+	$(libpath_run) test "`path_relto sub2/filename sub1`" = ../sub2/filename
+	$(libpath_run) echo "`path_relto / .`"
+	$(libpath_run) echo "`path_relto . /`"
+	$(libpath_run) echo "`path_relto / ..`"
+	$(libpath_run) echo "`path_relto .. /`"
