e30b6fb78d56f1e729cdc32ec7783f1f907ee52f
James Lott <james@lottspot.com>
2024-01-06 23:24:19 -0500

    tests/packlib-env.mk: refactor for lowercase locals convention

--- tests/packlib-env.mk
+++ tests/packlib-env.mk
@@ -1,18 +1,20 @@
+libenv_tests  += packlib-env-addfile
+libenv_tests  += packlib-env-addset
+libenv_tests  += packlib-env-setupseq
+libenv_tests  += packlib-env-exportseq
 SUITES        += packlib-env
-LIBENV_TESTS  += packlib-env-addfile
-LIBENV_TESTS  += packlib-env-addset
-LIBENV_TESTS  += packlib-env-setupseq
-LIBENV_TESTS  += packlib-env-exportseq
-TESTS         += $(LIBENV_TESTS)
+TESTS         += $(libenv_tests)
 
-packlib-env: $(LIBENV_TESTS)
+libenv_run := source ../install.sh &&
+
+packlib-env: $(libenv_tests)
 
 packlib-env-addfile:
-	source ../install.sh && env_file_add ./noop.env && test "$$ENV_FILES" = ./noop.env
+	$(libenv_run) env_file_add ./noop.env && test "$$ENV_FILES" = ./noop.env
 
 packlib-env-addset:
-	source ../install.sh && env_str_add FOO=BAR && test "$$ENV_STRS" = FOO=BAR
+	$(libenv_run) env_str_add FOO=BAR && test "$$ENV_STRS" = FOO=BAR
 
 packlib-env-setupseq:
-	source ../install.sh && ENV_STRS=(FOO=QUX) && ENV_FILES=(packlib-env.1.env packlib-env.2.env) && env_seq_setup && test $${ENV_SEQ[0]} = FOO=BAR && test $${ENV_SEQ[3]} = FOO=QUX
-	source ../install.sh && ENV_STRS=(FOO=QUX) && ENV_FILES=(packlib-env.1.env packlib-env.2.env) && env_seq_setup && for e in "$${ENV_SEQ[@]}"; do declare -x "$$e"; done && test "$$FOO" = QUX && test "$$HELLO" = WORLD
+	$(libenv_run) ENV_STRS=(FOO=QUX) && ENV_FILES=(packlib-env.1.env packlib-env.2.env) && env_seq_setup && test $${ENV_SEQ[0]} = FOO=BAR && test $${ENV_SEQ[3]} = FOO=QUX
+	$(libenv_run) ENV_STRS=(FOO=QUX) && ENV_FILES=(packlib-env.1.env packlib-env.2.env) && env_seq_setup && for e in "$${ENV_SEQ[@]}"; do declare -x "$$e"; done && test "$$FOO" = QUX && test "$$HELLO" = WORLD
