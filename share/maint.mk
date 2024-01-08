9c1b492f213291972a37bf1cc74780a225b463af
James Lott <james@lottspot.com>
2024-01-08 16:29:28 -0500

    share: add maintainer infra resources

---
+++ share/maint.mk
@@ -0,0 +1,26 @@
+DIST_FORMAT ?= tar
+
+PROG        := git-%pack_name%
+VERSION     := $(shell git describe --tags --always HEAD)
+RELEASE     := $(PROG)-$(VERSION)
+DIST_NAME   := $(RELEASE).$(DIST_FORMAT)
+
+dist: $(DIST_NAME)
+
+check:
+	$(MAKE) -C tests suites
+
+clean:
+	rm -f *.{tar,zip,tar.gz,tgz}
+	$(MAKE) -C tests clean
+
+$(DIST_NAME):
+	git archive -o '$@' \
+		--worktree-attributes \
+		--format=$(DIST_FORMAT) \
+		--prefix=$(PROG)/ \
+		HEAD
+
+.PHONY: dist
+.PHONY: check
+.PHONY: clean
