bc6aceb93891ea2fa0ae88db7e1aafa25b8e2aeb
James Lott <james@lottspot.com>
2024-01-08 20:06:19 -0500

    tests/installproperties.mk: run all scripts using $(SHELL)
    This wires more code paths into the debug log when tests are run using
    SHELL='bash -x'

--- tests/installproperties.mk
+++ tests/installproperties.mk
@@ -5,9 +5,9 @@ TESTS          += $(INSTPROP_TESTS)
 installproperties: $(INSTPROP_TESTS)
 
 installproperties-defaults:
-	test "`../install.sh --get-property package.name`" = configpack
-	test "`../install.sh --get-property package.configsdir`" = configs
-	test "`../install.sh --get-property package.libexecdir`" = libexec
-	test "`../install.sh --get-property install.pre`" = ""
-	test "`../install.sh --get-property install.post`" = ""
-	grep -E 'global|local' <<< "`../install.sh --get-property install.scope`" >/dev/null
+	test "`$(SHELL) ../install.sh --get-property package.name`" = configpack
+	test "`$(SHELL) ../install.sh --get-property package.configsdir`" = configs
+	test "`$(SHELL) ../install.sh --get-property package.libexecdir`" = libexec
+	test "`$(SHELL) ../install.sh --get-property install.pre`" = ""
+	test "`$(SHELL) ../install.sh --get-property install.post`" = ""
+	grep -E 'global|local' <<< "`$(SHELL) ../install.sh --get-property install.scope`" >/dev/null
