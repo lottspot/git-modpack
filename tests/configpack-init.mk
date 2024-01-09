eb47755297d3b63403abc2e993b1a4299da31de3
James Lott <james@lottspot.com>
2024-01-09 10:42:13 -0500

    tests/configpack-init.mk: add inner-global test case

--- tests/configpack-init.mk
+++ tests/configpack-init.mk
@@ -1,6 +1,7 @@
 initcmd_tests        += configpack-init-outer-global
 initcmd_tests        += configpack-init-outer-local
 initcmd_tests        += configpack-init-inner-local
+initcmd_tests        += configpack-init-inner-global
 initcmd_inner_cache  += $(CURDIR)/.cache/configpack-init
 initcmd_outer_cache  += $(HOME)/.cache/git-configpack-tests
 
@@ -14,21 +15,24 @@ initcmd_gitconfig_path    := $(initcmd_inner_cache)/global.gitconfig
 initcmd_outer_global_pack := $(initcmd_outer_cache)/git-outer-global
 initcmd_outer_local_pack  := $(initcmd_inner_cache)/git-outer-local
 initcmd_inner_local_pack  := $(initcmd_local_toplevel)/git-inner-local
+initcmd_inner_global_pack := $(initcmd_local_toplevel)/git-inner-global
 
 configpack-init              : $(initcmd_tests)
 configpack-init-outer-global : $(initcmd_outer_global_pack)
 configpack-init-outer-local  : $(initcmd_outer_local_pack)
 configpack-init-outer-local  : $(initcmd_local_toplevel)
 configpack-init-inner-local  : $(initcmd_inner_local_pack)
+configpack-init-inner-global : $(initcmd_inner_global_pack)
 $(initcmd_tests)             : configpack-init-setup
 $(initcmd_inner_local_pack)  : $(initcmd_local_toplevel)
 
 initcmd_environ                := GIT_CONFIG_GLOBAL='$(initcmd_gitconfig_path)' GIT_DIR='$(initcmd_local_toplevel)/.git'
 initcmd_git                    := $(initcmd_environ) git
 initcmd_top_installsh          := $(initcmd_environ) $(SHELL) '$(CURDIR)/../install.sh'
-initcmd_outer_global_installsh := $(initcmd_environ) $(SHELL) '$(initcmd_outer_global_pack)/install.sh'
-initcmd_outer_local_installsh  := $(initcmd_environ) $(SHELL) '$(initcmd_outer_local_pack)/install.sh'
-initcmd_inner_local_installsh  := $(initcmd_environ) $(SHELL) '$(initcmd_inner_local_pack)/install.sh'
+initcmd_outer_global_installsh := $(initcmd_environ) $(SHELL) '$(initcmd_outer_global_pack)/install.sh' --global
+initcmd_outer_local_installsh  := $(initcmd_environ) $(SHELL) '$(initcmd_outer_local_pack)/install.sh' --local
+initcmd_inner_global_installsh := $(initcmd_environ) $(SHELL) '$(initcmd_inner_global_pack)/install.sh' --global
+initcmd_inner_local_installsh  := $(initcmd_environ) $(SHELL) '$(initcmd_inner_local_pack)/install.sh' --local
 
 configpack-init-outer-global:
 	( cd '$(initcmd_outer_cache)' && $(initcmd_outer_global_installsh) )
@@ -43,6 +47,13 @@ configpack-init-outer-local:
 	cd $(initcmd_local_toplevel) && stat -c %i "`$(initcmd_git) -C '$(initcmd_local_toplevel)' outer-local-packdir`" | xargs expr `stat -c %i '$(initcmd_outer_local_pack)'` = # test outer-local-packdir
 	cd $(initcmd_local_toplevel) && stat -c %i "`$(initcmd_git) -C '$(initcmd_local_toplevel)' outer-local-configsdir`" | xargs expr `stat -c %i '$(initcmd_outer_local_pack)/configs'` = # test outer-local-configsdir
 	cd $(initcmd_local_toplevel) && stat -c %i "`$(initcmd_git) -C '$(initcmd_local_toplevel)' outer-local-libexecdir`" | xargs expr `stat -c %i '$(initcmd_outer_local_pack)/libexec'` = # test outer-local-libexecdir
+
+configpack-init-inner-global:
+	( cd '$(initcmd_local_toplevel)' && $(initcmd_inner_global_installsh) )
+	$(initcmd_git) -C '$(initcmd_local_toplevel)' help-inner-global
+	cd $(initcmd_local_toplevel) && stat -c %i "`$(initcmd_git) -C '$(initcmd_local_toplevel)' inner-global-packdir`" | xargs expr `stat -c %i '$(initcmd_inner_global_pack)'` = # test embeddedpack-packdir
+	cd $(initcmd_local_toplevel) && stat -c %i "`$(initcmd_git) -C '$(initcmd_local_toplevel)' inner-global-configsdir`" | xargs expr `stat -c %i '$(initcmd_inner_global_pack)/configs'` = # test embeddedpack-configsdir
+	cd $(initcmd_local_toplevel) && stat -c %i "`$(initcmd_git) -C '$(initcmd_local_toplevel)' inner-global-libexecdir`" | xargs expr `stat -c %i '$(initcmd_inner_global_pack)/libexec'` = # test embeddedpack-libexecdir
 
 configpack-init-inner-local:
 	( cd '$(initcmd_local_toplevel)' && $(initcmd_inner_local_installsh) )
@@ -62,7 +73,7 @@ configpack-init-setup:
 $(initcmd_local_toplevel): configpack-init-setup
 	$(initcmd_git) init '$@'
 
-$(initcmd_outer_local_pack) $(initcmd_outer_global_pack) $(initcmd_inner_local_pack): configpack-init-setup
+$(initcmd_outer_local_pack) $(initcmd_outer_global_pack) $(initcmd_inner_local_pack) $(initcmd_inner_global_pack): configpack-init-setup
 	$(initcmd_environ) $(SHELL) '$(CURDIR)/../libexec/init.sh' --all-resources '$@'
 
 .PHONY: configpack-init-setup
