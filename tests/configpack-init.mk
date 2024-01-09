6e4082d6ef18472dbbe7705b6ab1ff076395aa20
James Lott <james@lottspot.com>
2024-01-09 13:08:44 -0500

    tests/configpack-init.mk: add reinstall tests

--- tests/configpack-init.mk
+++ tests/configpack-init.mk
@@ -2,6 +2,8 @@ initcmd_tests        += configpack-init-outer-global
 initcmd_tests        += configpack-init-outer-local
 initcmd_tests        += configpack-init-inner-local
 initcmd_tests        += configpack-init-inner-global
+initcmd_tests        += configpack-init-global-reinstall
+initcmd_tests        += configpack-init-local-reinstall
 initcmd_inner_cache  += $(CURDIR)/.cache/configpack-init
 initcmd_outer_cache  += $(HOME)/.cache/git-configpack-tests
 
@@ -17,21 +19,25 @@ initcmd_outer_local_pack  := $(initcmd_inner_cache)/git-outer-local
 initcmd_inner_local_pack  := $(initcmd_local_toplevel)/git-inner-local
 initcmd_inner_global_pack := $(initcmd_local_toplevel)/git-inner-global
 
-configpack-init              : $(initcmd_tests)
-configpack-init-outer-global : $(initcmd_outer_global_pack)
-configpack-init-outer-local  : $(initcmd_outer_local_pack)
-configpack-init-outer-local  : $(initcmd_local_toplevel)
-configpack-init-inner-local  : $(initcmd_inner_local_pack)
-configpack-init-inner-global : $(initcmd_inner_global_pack)
-$(initcmd_tests)             : configpack-init-setup
-$(initcmd_local_toplevel)    : configpack-init-setup
-$(initcmd_outer_global_pack) : configpack-init-setup
-$(initcmd_inner_global_pack) : configpack-init-setup
-$(initcmd_outer_local_pack)  : configpack-init-setup
-$(initcmd_inner_local_pack)  : configpack-init-setup
-$(initcmd_outer_local_pack)  : $(initcmd_local_toplevel)
-$(initcmd_inner_global_pack) : $(initcmd_local_toplevel)
-$(initcmd_inner_local_pack)  : $(initcmd_local_toplevel)
+configpack-init                  : $(initcmd_tests)
+configpack-init-outer-global     : $(initcmd_outer_global_pack)
+configpack-init-outer-local      : $(initcmd_outer_local_pack)
+configpack-init-outer-local      : $(initcmd_local_toplevel)
+configpack-init-inner-local      : $(initcmd_inner_local_pack)
+configpack-init-inner-global     : $(initcmd_inner_global_pack)
+configpack-init-global-reinstall : configpack-init-outer-global
+configpack-init-global-reinstall : configpack-init-inner-global
+configpack-init-local-reinstall  : configpack-init-outer-local
+configpack-init-local-reinstall  : configpack-init-inner-local
+$(initcmd_tests)                 : configpack-init-setup
+$(initcmd_local_toplevel)        : configpack-init-setup
+$(initcmd_outer_global_pack)     : configpack-init-setup
+$(initcmd_inner_global_pack)     : configpack-init-setup
+$(initcmd_outer_local_pack)      : configpack-init-setup
+$(initcmd_inner_local_pack)      : configpack-init-setup
+$(initcmd_outer_local_pack)      : $(initcmd_local_toplevel)
+$(initcmd_inner_global_pack)     : $(initcmd_local_toplevel)
+$(initcmd_inner_local_pack)      : $(initcmd_local_toplevel)
 
 initcmd_environ := GIT_CONFIG_GLOBAL='$(initcmd_gitconfig_path)' GIT_DIR='$(initcmd_local_toplevel)/.git'
 initcmd_git     := $(initcmd_environ) git
@@ -62,6 +68,14 @@ configpack-init-setup:
 	printf '[user]\nemail=foo@example.com\nname=Foo Bar\n' > '$(initcmd_gitconfig_path)'
 	$(initcmd_environ) $(SHELL) '$(CURDIR)/../install.sh'
 	$(initcmd_git) configpack-init -h | tee /dev/stderr | grep '^usage:' >/dev/null
+
+configpack-init-global-reinstall:
+	cd '$(initcmd_outer_cache)' && $(initcmd_environ) $(SHELL) '$(initcmd_outer_global_pack)'/install.sh --reinstall
+	cd '$(dir $(initcmd_gitconfig_path))' && while read p; do stat "$$p"; done < <($(initcmd_git) config --global --get-all include.path)
+
+configpack-init-local-reinstall:
+	cd '$(initcmd_local_toplevel)' && $(initcmd_environ) $(SHELL) '$(initcmd_outer_local_pack)'/install.sh --reinstall
+	cd '$(initcmd_local_toplevel)/.git' && while read p; do stat "$$p"; done < <($(initcmd_git) config --local --get-all include.path)
 
 $(initcmd_local_toplevel):
 	$(initcmd_git) init '$@'
